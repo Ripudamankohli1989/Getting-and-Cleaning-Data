a89bbe2dd6e682f2c85bc508d13b8eea1c0a4ae8
Ripudaman Singh Kohli <Ripudamankohli1989@gmail.com>
2015-03-22 19:11:18 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -7,11 +7,11 @@ rm(list=ls())
 setwd('/Users/heather/Documents/UCI HAR Dataset/');
 
 # Read in the data from files
-features     = read.table('./features.txt',header=FALSE); #imports features.txt
-activityType = read.table('./activity_labels.txt',header=FALSE); #imports activity_labels.txt
-subjectTrain = read.table('./train/subject_train.txt',header=FALSE); #imports subject_train.txt
-xTrain       = read.table('./train/x_train.txt',header=FALSE); #imports x_train.txt
-yTrain       = read.table('./train/y_train.txt',header=FALSE); #imports y_train.txt
+features     = read.table('C:/Users/Ripudaman/Downloads/Study/CourseEra/3_Getting_and_Cleaning_data/Week_3/UCI HAR Dataset/features.txt',header=FALSE); #imports features.txt
+activityType = read.table('C:/Users/Ripudaman/Downloads/Study/CourseEra/3_Getting_and_Cleaning_data/Week_3/UCI HAR Dataset/activity_labels.txt',header=FALSE); #imports activity_labels.txt
+subjectTrain = read.table('C:/Users/Ripudaman/Downloads/Study/CourseEra/3_Getting_and_Cleaning_data/Week_3/UCI HAR Dataset/train/subject_train.txt',header=FALSE); #imports subject_train.txt
+xTrain       = read.table('C:/Users/Ripudaman/Downloads/Study/CourseEra/3_Getting_and_Cleaning_data/Week_3/UCI HAR Dataset/train/x_train.txt',header=FALSE); #imports x_train.txt
+yTrain       = read.table('C:/Users/Ripudaman/Downloads/Study/CourseEra/3_Getting_and_Cleaning_data/Week_3/UCI HAR Dataset/train/y_train.txt',header=FALSE); #imports y_train.txt
 
 # Assigin column names to the data imported above
 colnames(activityType)  = c('activityId','activityType');
@@ -23,9 +23,9 @@ colnames(yTrain)        = "activityId";
 trainingData = cbind(yTrain,subjectTrain,xTrain);
 
 # Read in the test data
-subjectTest = read.table('./test/subject_test.txt',header=FALSE); #imports subject_test.txt
-xTest       = read.table('./test/x_test.txt',header=FALSE); #imports x_test.txt
-yTest       = read.table('./test/y_test.txt',header=FALSE); #imports y_test.txt
+subjectTest = read.table('C:/Users/Ripudaman/Downloads/Study/CourseEra/3_Getting_and_Cleaning_data/Week_3/UCI HAR Dataset/test/subject_test.txt',header=FALSE); #imports subject_test.txt
+xTest       = read.table('C:/Users/Ripudaman/Downloads/Study/CourseEra/3_Getting_and_Cleaning_data/Week_3/UCI HAR Dataset/test/x_test.txt',header=FALSE); #imports x_test.txt
+yTest       = read.table('C:/Users/Ripudaman/Downloads/Study/CourseEra/3_Getting_and_Cleaning_data/Week_3/UCI HAR Dataset/test/y_test.txt',header=FALSE); #imports y_test.txt
 
 # Assign column names to the test data imported above
 colnames(subjectTest) = "subjectId";
@@ -94,4 +94,4 @@ tidyData    = aggregate(finalDataNoActivityType[,names(finalDataNoActivityType) 
 tidyData    = merge(tidyData,activityType,by='activityId',all.x=TRUE);
 
 # Export the tidyData set 
-write.table(tidyData, './tidyData.txt',row.names=TRUE,sep='\t');
+write.table(tidyData, 'C:/Users/Ripudaman/Downloads/Study/CourseEra/3_Getting_and_Cleaning_data/Week_3/UCI HAR Dataset/tidyData.txt',row.names=TRUE,sep='\t');
